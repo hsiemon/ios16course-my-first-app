12c94dfca472229013b1b31b924980fde015f513
Henrique Siemon <henriquesiemon@msn.com>
2023-07-30 16:32:53 -0300

    Lecture 10 - connecting views

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var heroImage: UIImageView!
+    @IBOutlet weak var titleLable: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeImage(_ sender: Any) {
+    }
+    
 }
 
